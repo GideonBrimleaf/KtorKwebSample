8c6c905eec881887f6f0e5e72ff51039bf4f563f
GideonBrimleaf <chrisdryden1@gmail.com>
2021-01-05 21:49:39 +0000

    websockets for kweb async response added

--- src/Application.kt
+++ src/Application.kt
@@ -1,12 +1,22 @@
 package com.example
 
+import freemarker.cache.ClassTemplateLoader
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import freemarker.cache.*
+import io.ktor.features.*
 import io.ktor.freemarker.*
+import io.ktor.http.*
+import io.ktor.http.cio.websocket.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kweb.Kweb
+import kweb.h1
+import kweb.respondKweb
+import kweb.state.KVar
+import java.time.Duration
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -17,13 +27,35 @@ fun Application.module(testing: Boolean = false) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
     }
 
+    install(DefaultHeaders)
+    install(Compression)
+    install(WebSockets) {
+        pingPeriod = Duration.ofSeconds(10)
+        timeout = Duration.ofSeconds(30)
+    }
+    install(Kweb)
+
+
     routing {
         get("/") {
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
         }
 
-        get("/html-freemarker") {
+        get("/freemarker-async-response") {
             call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
+        }
+
+        get("/kweb-async-response") {
+            call.respondKweb {
+                doc.body {
+                    val name = KVar("Loading")
+                    GlobalScope.launch {
+                        delay(3000L)
+                        name.value = "Han Solo"
+                    }
+                    h1().text(name.map{ "The name is $it" })
+                }
+            }
         }
     }
 }
